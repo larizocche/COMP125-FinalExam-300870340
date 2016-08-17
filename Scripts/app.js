c62059dd8b1ab7fa0771e38c71eb7d75fce0ea5a
larizocche <lari.zocche@gmail.com>
2016-08-17 12:48:28 -0400

    insert info

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,21 @@
+/**
+ * FileName: app.js
+ * 
+ * @author Larissa Zocche
+ * @date August 17, 2016
+ * 
+ * StudentID: 300870340
+ * 
+ * @description This file is the main javascript file for the web site
+ */
+
+
 var core;
 (function (core) {
     "use strict";
     var canvas;
     var stage;
+    
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
